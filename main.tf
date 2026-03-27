eb29f59c7165d137f551490ff1d6af3b2fc3a60c
Felipe Jediel S <69176862+FelipeJediel@users.noreply.github.com>
2026-03-27 12:37:37 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,21 +7,21 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Permite SSH de qualquer IP
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Permite HTTP de qualquer IP
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]  # Permite saída para qualquer destino
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 }
 
