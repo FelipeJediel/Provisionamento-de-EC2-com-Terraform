0562a617697aed80a41deaad2428ee2efbdafe5e
Felipe Jediel S <69176862+FelipeJediel@users.noreply.github.com>
2026-03-27 12:52:33 -0300

    Delete user_data.sh

--- user_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "Desafio DevOps #1 — Deploy realizado com sucesso!" > /var/www/html/index.html
